910ff10c357f3432284a3ed26f11321ca41c1cd8
Nikhil4474 <ninuganti15@gmail.com>
2022-10-14 20:49:50 -0500

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import DrawerNavigator from './navigation/DrawerNavigator';
+import TabNavigator from './navigation/TabNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TabNavigator>
+      <DrawerNavigator/>
+    </TabNavigator>
   );
 }
 
